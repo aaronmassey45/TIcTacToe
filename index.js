214c6dca2747465cb4a913d2108f90ef952fa642
Aaron <aaronmassey45@gmail.com>
2018-04-17 09:32:30 -0500

    Renamed almostWin to chooseAiSquare and condensed func

--- index.js
+++ index.js
@@ -2,14 +2,22 @@ $(document).ready(function() {
   let grid = Array(9).fill(null);
   const p1 = { xo: '', winCount: 0 };
   const p2 = { xo: '', winCount: 0, isComputer: false, isComputerTurn: false };
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
   var numPlayers = 0; //stores number of players
   var turn = 0; //turn counter
   var prefix = 'grid';
   var el; //used with prefix to set mark on a grid position
   var empty = ['6', '2', '0', '1', '3', '4', '8', '5', '7']; //array for empty grid spaces
   var lastDigit; //gets the digit off grid id
-  var p1Mark = [],
-    p2Mark = []; //stores p1 and p2/AI's marks
   var win; //variable to stop game if there is a win or draw
   var turnTime = 350;
 
@@ -68,47 +76,22 @@ $(document).ready(function() {
         p2.isComputerTurn = false;
         $(this).html(p1.xo); //sets p1 marker at the clicked location
         turn++; //increments turn
+        lastDigit = this.id.substr(-1); //grabs digit from grid id
+        grid[lastDigit] = p1.xo;
         checkWin(grid); //checks to see if p1 has won
         if (win === true) {
           return;
         }
-        $('.playerOneTurn').animate(
-          {
-            width: 'toggle',
-          },
-          turnTime
-        );
-        $('.playerTwoTurn').animate(
-          {
-            width: 'toggle',
-          },
-          turnTime
-        );
+        $('.playerOneTurn').animate({ width: 'toggle' }, turnTime);
+        $('.playerTwoTurn').animate({ width: 'toggle' }, turnTime);
         $('#gameboard').css('pointer-events', 'none'); //disables clicking during computer turn
-        lastDigit = this.id.substr(-1); //grabs digit from grid id
         setTimeout(function() {
-          grid[lastDigit] = p1.xo;
-          //p1Mark.push(lastDigit); //adds grid location to array of p1 marks
-          empty.splice(empty.indexOf(lastDigit), 1); //removes the clicked location from array of empty spots
-          almostWin(); //checks to see if user/AI has almost won before AI chooses a spot and if not sets computerGo to 1
-          if (p2.isComputerTurn === true) {
-            //denotes that it is the AI's turn and user/AI is not in a position to win
-            el = document.getElementById(prefix + empty[0]); //sets el as the id of the first grid space in empty array
-            $(el).html(p2.xo); //puts AI mark at el
-            grid[empty.shift()] = p2.xo;
-          }
-          $('.playerOneTurn').animate(
-            {
-              width: 'toggle',
-            },
-            turnTime
-          );
-          $('.playerTwoTurn').animate(
-            {
-              width: 'toggle',
-            },
-            turnTime
-          );
+          const spot = chooseAiSquare(grid);
+          el = document.getElementById(prefix + spot); //sets el
+          $(el).html(p2.xo); //puts AI mark at el
+          grid[spot] = p2.xo;
+          $('.playerOneTurn').animate({ width: 'toggle' }, turnTime);
+          $('.playerTwoTurn').animate({ width: 'toggle' }, turnTime);
           $('#gameboard').css('pointer-events', 'auto'); //re-enables clicking after computer turn
           turn++; //increments turn
           checkWin(grid); //checks to see if AI has won
@@ -165,16 +148,6 @@ $(document).ready(function() {
 
   //checkWin FUNCTION
   function checkWin(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
       if (
@@ -189,385 +162,26 @@ $(document).ready(function() {
   }
 
   //Chooses where AI marks on the grid if p1 or AI is about to win
-  function almostWin() {
-    //check AI's positions first
-    if (p2Mark.includes('0') && p2Mark.includes('1') && empty.includes('2')) {
-      $('#grid2').html(p2.xo);
-    } else if (
-      p2Mark.includes('0') &&
-      p2Mark.includes('2') &&
-      empty.includes('1')
-    ) {
-      $('#grid1').html(p2.xo);
-    } else if (
-      p2Mark.includes('0') &&
-      p2Mark.includes('3') &&
-      empty.includes('6')
-    ) {
-      $('#grid6').html(p2.xo);
-    } else if (
-      p2Mark.includes('0') &&
-      p2Mark.includes('4') &&
-      empty.includes('8')
-    ) {
-      $('#grid8').html(p2.xo);
-    } else if (
-      p2Mark.includes('0') &&
-      p2Mark.includes('6') &&
-      empty.includes('3')
-    ) {
-      $('#grid3').html(p2.xo);
-    } else if (
-      p2Mark.includes('0') &&
-      p2Mark.includes('8') &&
-      empty.includes('4')
-    ) {
-      $('#grid4').html(p2.xo);
+  function chooseAiSquare(squares) {
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[c] === null) {
+        return c;
+      } else if (
+        squares[a] &&
+        squares[a] === squares[c] &&
+        squares[b] === null
+      ) {
+        return b;
+      } else if (
+        squares[b] &&
+        squares[b] === squares[c] &&
+        squares[a] === null
+      ) {
+        return a;
+      }
     }
-
-    //1's
-    else if (
-      p2Mark.includes('1') &&
-      p2Mark.includes('2') &&
-      empty.includes('0')
-    ) {
-      $('#grid0').html(p2.xo);
-    } else if (
-      p2Mark.includes('1') &&
-      p2Mark.includes('4') &&
-      empty.includes('7')
-    ) {
-      $('#grid7').html(p2.xo);
-    } else if (
-      p2Mark.includes('1') &&
-      p2Mark.includes('7') &&
-      empty.includes('4')
-    ) {
-      $('#grid4').html(p2.xo);
-    }
-
-    //2's
-    else if (
-      p2Mark.includes('2') &&
-      p2Mark.includes('6') &&
-      empty.includes('4')
-    ) {
-      $('#grid4').html(p2.xo);
-    } else if (
-      p2Mark.includes('2') &&
-      p2Mark.includes('4') &&
-      empty.includes('6')
-    ) {
-      $('#grid6').html(p2.xo);
-    } else if (
-      p2Mark.includes('2') &&
-      p2Mark.includes('5') &&
-      empty.includes('8')
-    ) {
-      $('#grid8').html(p2.xo);
-    } else if (
-      p2Mark.includes('2') &&
-      p2Mark.includes('8') &&
-      empty.includes('5')
-    ) {
-      $('#grid5').html(p2.xo);
-    }
-
-    //3's
-    else if (
-      p2Mark.includes('3') &&
-      p2Mark.includes('4') &&
-      empty.includes('5')
-    ) {
-      $('#grid5').html(p2.xo);
-    } else if (
-      p2Mark.includes('3') &&
-      p2Mark.includes('5') &&
-      empty.includes('4')
-    ) {
-      $('#grid4').html(p2.xo);
-    } else if (
-      p2Mark.includes('3') &&
-      p2Mark.includes('6') &&
-      empty.includes('0')
-    ) {
-      $('#grid0').html(p2.xo);
-    }
-
-    //4's
-    else if (
-      p2Mark.includes('4') &&
-      p2Mark.includes('5') &&
-      empty.includes('3')
-    ) {
-      $('#grid3').html(p2.xo);
-    } else if (
-      p2Mark.includes('4') &&
-      p2Mark.includes('6') &&
-      empty.includes('2')
-    ) {
-      $('#grid2').html(p2.xo);
-    } else if (
-      p2Mark.includes('4') &&
-      p2Mark.includes('7') &&
-      empty.includes('1')
-    ) {
-      $('#grid1').html(p2.xo);
-    } else if (
-      p2Mark.includes('4') &&
-      p2Mark.includes('8') &&
-      empty.includes('0')
-    ) {
-      $('#grid0').html(p2.xo);
-    }
-
-    //6
-    else if (
-      p2Mark.includes('6') &&
-      p2Mark.includes('7') &&
-      empty.includes('8')
-    ) {
-      $('#grid8').html(p2.xo);
-    }
-
-    //8's
-    else if (
-      p2Mark.includes('8') &&
-      p2Mark.includes('5') &&
-      empty.includes('2')
-    ) {
-      $('#grid2').html(p2.xo);
-    } else if (
-      p2Mark.includes('8') &&
-      p2Mark.includes('6') &&
-      empty.includes('7')
-    ) {
-      $('#grid7').html(p2.xo);
-    } else if (
-      p2Mark.includes('8') &&
-      p2Mark.includes('7') &&
-      empty.includes('6')
-    ) {
-      $('#grid6').html(p2.xo);
-    }
-
-    //check p1's positions
-    else if (
-      p1Mark.includes('0') &&
-      p1Mark.includes('1') &&
-      empty.includes('2')
-    ) {
-      $('#grid2').html(p2.xo);
-      empty.splice(empty.indexOf('2'), 1);
-      p2Mark.push('2');
-    } else if (
-      p1Mark.includes('0') &&
-      p1Mark.includes('2') &&
-      empty.includes('1')
-    ) {
-      $('#grid1').html(p2.xo);
-      empty.splice(empty.indexOf('1'), 1);
-      p2Mark.push('1');
-    } else if (
-      p1Mark.includes('0') &&
-      p1Mark.includes('3') &&
-      empty.includes('6')
-    ) {
-      $('#grid6').html(p2.xo);
-      empty.splice(empty.indexOf('6'), 1);
-      p2Mark.push('6');
-    } else if (
-      p1Mark.includes('0') &&
-      p1Mark.includes('4') &&
-      empty.includes('8')
-    ) {
-      $('#grid8').html(p2.xo);
-      empty.splice(empty.indexOf('8'), 1);
-      p2Mark.push('8');
-    } else if (
-      p1Mark.includes('0') &&
-      p1Mark.includes('6') &&
-      empty.includes('3')
-    ) {
-      $('#grid3').html(p2.xo);
-      empty.splice(empty.indexOf('3'), 1);
-      p2Mark.push('3');
-    } else if (
-      p1Mark.includes('0') &&
-      p1Mark.includes('8') &&
-      empty.includes('4')
-    ) {
-      $('#grid4').html(p2.xo);
-      empty.splice(empty.indexOf('4'), 1);
-      p2Mark.push('4');
-    }
-
-    //1's
-    else if (
-      p1Mark.includes('1') &&
-      p1Mark.includes('2') &&
-      empty.includes('0')
-    ) {
-      $('#grid0').html(p2.xo);
-      empty.splice(empty.indexOf('0'), 1);
-      p2Mark.push('0');
-    } else if (
-      p1Mark.includes('1') &&
-      p1Mark.includes('4') &&
-      empty.includes('7')
-    ) {
-      $('#grid7').html(p2.xo);
-      empty.splice(empty.indexOf('7'), 1);
-      p2Mark.push('7');
-    } else if (
-      p1Mark.includes('1') &&
-      p1Mark.includes('7') &&
-      empty.includes('4')
-    ) {
-      $('#grid4').html(p2.xo);
-      empty.splice(empty.indexOf('4'), 1);
-      p2Mark.push('4');
-    }
-
-    //2's
-    else if (
-      p1Mark.includes('2') &&
-      p1Mark.includes('6') &&
-      empty.includes('4')
-    ) {
-      $('#grid4').html(p2.xo);
-      empty.splice(empty.indexOf('4'), 1);
-      p2Mark.push('4');
-    } else if (
-      p1Mark.includes('2') &&
-      p1Mark.includes('4') &&
-      empty.includes('6')
-    ) {
-      $('#grid6').html(p2.xo);
-      empty.splice(empty.indexOf('6'), 1);
-      p2Mark.push('6');
-    } else if (
-      p1Mark.includes('2') &&
-      p1Mark.includes('5') &&
-      empty.includes('8')
-    ) {
-      $('#grid8').html(p2.xo);
-      empty.splice(empty.indexOf('8'), 1);
-      p2Mark.push('8');
-    } else if (
-      p1Mark.includes('2') &&
-      p1Mark.includes('8') &&
-      empty.includes('5')
-    ) {
-      $('#grid5').html(p2.xo);
-      empty.splice(empty.indexOf('5'), 1);
-      p2Mark.push('5');
-    }
-
-    //3's
-    else if (
-      p1Mark.includes('3') &&
-      p1Mark.includes('4') &&
-      empty.includes('5')
-    ) {
-      $('#grid5').html(p2.xo);
-      empty.splice(empty.indexOf('5'), 1);
-      p2Mark.push('5');
-    } else if (
-      p1Mark.includes('3') &&
-      p1Mark.includes('5') &&
-      empty.includes('4')
-    ) {
-      $('#grid4').html(p2.xo);
-      empty.splice(empty.indexOf('4'), 1);
-      p2Mark.push('4');
-    } else if (
-      p1Mark.includes('3') &&
-      p1Mark.includes('6') &&
-      empty.includes('0')
-    ) {
-      $('#grid0').html(p2.xo);
-      empty.splice(empty.indexOf('0'), 1);
-      p2Mark.push('0');
-    }
-
-    //4's
-    else if (
-      p1Mark.includes('4') &&
-      p1Mark.includes('5') &&
-      empty.includes('3')
-    ) {
-      $('#grid3').html(p2.xo);
-      empty.splice(empty.indexOf('3'), 1);
-      p2Mark.push('3');
-    } else if (
-      p1Mark.includes('4') &&
-      p1Mark.includes('6') &&
-      empty.includes('2')
-    ) {
-      $('#grid2').html(p2.xo);
-      empty.splice(empty.indexOf('2'), 1);
-      p2Mark.push('2');
-    } else if (
-      p1Mark.includes('4') &&
-      p1Mark.includes('7') &&
-      empty.includes('1')
-    ) {
-      $('#grid1').html(p2.xo);
-      empty.splice(empty.indexOf('1'), 1);
-      p2Mark.push('1');
-    } else if (
-      p1Mark.includes('4') &&
-      p1Mark.includes('8') &&
-      empty.includes('0')
-    ) {
-      $('#grid0').html(p2.xo);
-      empty.splice(empty.indexOf('0'), 1);
-      p2Mark.push('0');
-    }
-
-    //6
-    else if (
-      p1Mark.includes('6') &&
-      p1Mark.includes('7') &&
-      empty.includes('8')
-    ) {
-      $('#grid8').html(p2.xo);
-      empty.splice(empty.indexOf('8'), 1);
-      p2Mark.push('8');
-    }
-
-    //8's
-    else if (
-      p1Mark.includes('8') &&
-      p1Mark.includes('5') &&
-      empty.includes('2')
-    ) {
-      $('#grid2').html(p2.xo);
-      empty.splice(empty.indexOf('2'), 1);
-      p2Mark.push('2');
-    } else if (
-      p1Mark.includes('8') &&
-      p1Mark.includes('6') &&
-      empty.includes('7')
-    ) {
-      $('#grid7').html(p2.xo);
-      empty.splice(empty.indexOf('7'), 1);
-      p2Mark.push('7');
-    } else if (
-      p1Mark.includes('8') &&
-      p1Mark.includes('7') &&
-      empty.includes('6')
-    ) {
-      $('#grid6').html(p2.xo);
-      empty.splice(empty.indexOf('6'), 1);
-      p2Mark.push('6');
-    }
-
-    // if none
-    else {
-      p2.isComputerTurn = true;
-    }
+    return empty.shift();
   }
 
   //RESET FUNCTION TO RESET GAME AFTER A WIN OR DRAW
@@ -576,8 +190,6 @@ $(document).ready(function() {
     turn = 0;
     grid = Array(9).fill(null);
     empty = ['6', '2', '0', '1', '3', '4', '8', '5', '7'];
-    p1Mark = [];
-    p2Mark = [];
 
     for (i = 0; (el = document.getElementById(prefix + i)); i++) {
       $(el).html('');
@@ -601,11 +213,8 @@ $(document).ready(function() {
     turn = 0;
     p1.winCount = 0;
     p2.winCount = 0;
-    computerWinCount = 0;
     grid = Array(9).fill(null);
     empty = ['6', '2', '0', '1', '3', '4', '8', '5', '7'];
-    p1Mark = [];
-    p2Mark = [];
 
     $('.p1WinCount').html('0');
     $('.aiORp2WinCount').html('0');
@@ -688,8 +297,8 @@ $(document).ready(function() {
           .html('Computer wins!')
           .fadeIn()
           .fadeOut(1000);
-        computerWinCount++;
-        $('.aiORp2WinCount').html(computerWinCount);
+        p2.winCount++;
+        $('.aiORp2WinCount').html(p2.winCount);
       } else if (numPlayers === 2) {
         $('#winner')
           .html('Player 2 wins!')
